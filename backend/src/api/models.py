b6adf4365783714eb023041ac9d008159c1dd3d8
Matt Nyandjo <mattn3@umbc.edu>
2026-04-11 20:24:43 -0400

    Models for inventory item

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -13,3 +13,33 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class InventoryItem(models.Model):
+    ITEM_TYPES = [
+    ("material", "Material"),
+    ("equipment", "Equipment"),
+    ]
+
+    item_id = models.AutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    type = models.CharField(
+    max_length=20,
+    choices=ITEM_TYPES
+    )
+
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    supplier = models.CharField(max_length=255)
+
+    created_by = models.ForeignKey(
+    CustomUser,
+    on_delete=models.CASCADE,
+    related_name="items"
+    )
+
+    def __str__(self):
+        return self.name
